5a311e6b13d387293b91fdf19864b3cd50002ccc
Kirill Titov <machina91@mail.ru>
2021-10-24 01:16:54 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let newImg = document.querySelector('#newImg');
 function show() {
     if(bigImage.querySelector('img')) {
         console.log('yes');
-        newImg.setAttribute('src', './img/nature-1.jpg');
+        newImg.setAttribute('src', 'img/nature-1.jpg');
         newImg.setAttribute('alt', 'nature');
         newImg.setAttribute('data-nature', "1");
         newImg.classList.add('img-start');
